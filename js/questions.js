5f4be111a3689aa4298cc9ef3d9f93053849cc6a
jghall23 <114723936+jghall23@users.noreply.github.com>
2022-11-08 00:07:59 -0500

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What does Javascrpt do?",
+    answer: "Makes Webpages Interactive",
+    options: [
+      "CSS",
+      "Makes Webpages Interactive",
+      "EVerything",
+      "Nothing"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does CSS Do?",
+    answer: "The language that presents Web Pages",
+    options: [
+      "Nothing",
+      "The language that presents Web Pages",
+      "Hacking Tool",
+      "N/A"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What does Jquery do?",
+    answer: "makes Javascipt easier",
+    options: [
+      "makes Javascript easier",
+      "makes CSS easier",
+      "Nothing",
+      "Makes HTML easier"
+    ]
+  },
+
+];
